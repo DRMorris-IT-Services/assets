6c39ef5a50bc0d0cd5659925a1a66b9a5f10fb9e
Duncan Morris <d.morris@drmorris-itservices.de>
2020-10-21 15:18:05 +0200

    #1 database tables setup

--- src/migrations/2020_10_20_062227_create_assets_table.php
+++ src/migrations/2020_10_20_062227_create_assets_table.php
@@ -17,6 +17,14 @@ class CreateAssetsTable extends Migration
             $table->id();
             $table->string('asset_id');
             $table->string('asset_name')->nullable();
+            $table->string('asset_model')->nullable();
+            $table->string('asset_serial_no')->nullable();
+            $table->string('asset_tag_no')->nullable();
+            $table->date('asset_purchase_date')->nullable();
+            $table->date('asset_warranty_date')->nullable();
+            $table->string('asset_assigned_to')->nullable();
+            $table->string('asset_location')->nullable();
+            $table->string('asset_software')->nullable();
             
             $table->timestamps();
         });
